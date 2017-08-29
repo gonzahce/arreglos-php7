2d12219b93f9a85a6424042c2ef8946510293509
Carlos Gonzalez <carlosegonzax@gmail.com>
2017-08-28 21:27:59 -0400

    v.1

--- app/index.php
+++ app/index.php
@@ -1,46 +1,46 @@
 <?php
 
-	require '../vendor/autoload.php';
-	/*
+	require('../vendor/autoload.php');
+	
 	$usuario = [
 		['nombre' => 'Jose', 'score' => 55],
 		['nombre' => 'Luis', 'score' => 100],
 		['nombre' => 'Carlos', 'score' => 400],
 		['nombre' => 'Daniel', 'score' => 1],
 	];
-	*/
+	
 
 	#dd(array_last($usuario));
 
-	/*
+	
 	#getting the callback to reverse
 	dd(array_last($usuario, function($usuario, $key){
 		return array_get($usuario, 'score') == 55;
 	}));
 	
 
-	dd(array_where($usuario, function($usuario, $key){
-		return array_get($usuario, 'score') >= 20;
-	}));
-	*/
+	// dd(array_where($usuario, function($usuario, $key){
+	// 	return array_get($usuario, 'score') >= 20;
+	// }));
+	
 
-	$usuario = [
-		'nombre' => 'carlos',
-		'temas' => [
-			['title' => 'tema uno'],
-			['title' => 'tema dos'],
-		],
-		'pais' => [
-			'nombre' => 'vzla',
-			'bandera' => [
-				'url' => 'ruta.png',
-				'tamano' => 32
-			]
-		]
-	];
+	// $usuario = [
+	// 	'nombre' => 'carlos',
+	// 	'temas' => [
+	// 		['title' => 'tema uno'],
+	// 		['title' => 'tema dos'],
+	// 	],
+	// 	'pais' => [
+	// 		'nombre' => 'vzla',
+	// 		'bandera' => [
+	// 			'url' => 'ruta.png',
+	// 			'tamano' => 32
+	// 		]
+	// 	]
+	// ];
 
-	#dd(array_only($usuario, ['nombre', 'pais']));
+	//dd(array_only($usuario, ['nombre', 'pais']));
 
-	dd(array_has($usuario, ['nombre', 'pais']));
+	//dd(array_has($usuario, ['nombre', 'pais']));
 
 ?>
